c4c251e49f02745f68c41749a7e9b5acd035370a
mhamid019 <yasirnadeem0199@gmail.com>
2025-09-14 11:01:54 +0530

    submit btn functionality added

--- js/index.js
+++ js/index.js
@@ -3,3 +3,8 @@ let wrap1 = document.querySelector(".wrap1");
 myBtn.addEventListener("click", function() {
     wrap1.classList.toggle("add-bg-color");
 });
+
+const submitBtn = document.querySelector("#submitBtn");
+submitBtn.addEventListener("click", function() {
+    alert("Thanks for contacting with us.")
+});
